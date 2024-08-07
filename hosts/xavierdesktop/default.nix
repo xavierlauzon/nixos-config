e33defe2db7453d50d8a085f75c5e07743b7c4db
xavierlauzon <156852256+xavierlauzon@users.noreply.github.com>
2024-08-07 16:39:11 -0400

    edit role

--- hosts/xavierdesktop/default.nix
+++ hosts/xavierdesktop/default.nix
@@ -8,7 +8,26 @@
 
   host = {
     feature = {
-    };
+      appimage.enable = true;
+      gaming = {
+        gamemode.enable = false;
+        gamescope.enable = false;
+        heroic.enable = false;
+        steam = {
+          enable = false;
+          protonGE = false;
+        };
+      };
+      graphics = {
+        enable = true;
+        backend = "wayland";
+        displayManager.manager = "sddm";
+        windowManager.manager = "hyprland";
+      };
+      virtualization = {
+        flatpak.enable = true;
+        waydroid.enable = true;
+      };
     filesystem = {
       encryption.enable = false;                 # This line can be removed if not needed as it is already default set by the role template
       impermanence.enable = false;               # This line can be removed if not needed as it is already default set by the role template
@@ -16,18 +35,23 @@
         partition = "disk/by-partlabel/swap";
       };
     };
+  };
     hardware = {
       cpu = "amd";
-      raid.enable = false;                      # This line can be removed if not needed as it is already default set by the role template
+      gpu = "amd";
+      raid.enable = false;  
+      sound = {
+        server = "pipewire";
+      };
     };
     network = {
       hostname = "xavierdesktop";
       wired = {
        enable = true;
        type = "dynamic";
-};
+      };
     };
-    role = "server";
+    role = "desktop";
     user = {
       root.enable = true;
       xavier.enable = true;
