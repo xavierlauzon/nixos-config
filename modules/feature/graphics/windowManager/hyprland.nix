03e4c5e4b7598ade6e4ca48a2efce2c5af932081
Xavier Lauzon <xavier@lumae.net>
2024-09-04 11:37:28 -0400

    nixos-unstable broken so switch hyprland to upstream flake

--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -9,7 +9,8 @@ in
     programs = {
       hyprland = {
         enable = mkDefault true;
-#        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
     };
 
