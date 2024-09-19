7e5485359c85559775930a4a7ca004d4216ca09f
Xavier Lauzon <xavier@lumae.net>
2024-09-18 21:17:47 -0400

    more portal!

--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -26,6 +26,7 @@ in
       extraPortals = [
         inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-wlr
       ];
     };
   };
