6cb693d4a4227356f512a2da93442fa3e9d2adb9
Xavier Lauzon <xavier@lumae.net>
2024-11-07 12:15:48 -0500

    update flake inputs

--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -11,8 +11,12 @@ in
         enable = mkDefault true;
         package = pkgs.hyprland;
         portalPackage = pkgs.xdg-desktop-portal-hyprland;
+#        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+#        portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
       };
     };
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     xdg.portal = {
       enable = true;
@@ -21,12 +25,14 @@ in
         "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
         "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
         "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
-        "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
+        "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-shana" ];
       };
       extraPortals = [
         pkgs.xdg-desktop-portal-hyprland
+#        inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-shana
       ];
     };
   };
