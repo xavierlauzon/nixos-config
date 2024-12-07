e10703b6f59b387a98b1f86088218f80c5b49a7e
Xavier Lauzon <xavier@lumae.net>
2024-12-07 17:16:17 -0500

    Polished configuration for ms1 & ms2

---
+++ hosts/ms1/default.nix
@@ -0,0 +1,79 @@
+{ config, inputs, pkgs, ...}: {
+
+  imports = [
+    inputs.disko.nixosModules.disko
+    ./disks.nix
+    ../common
+  ];
+
+  host = {
+    feature = {
+      boot = {
+        initrd = {
+          modules = [ "xhci_pci" "ehci_pci" "uhci_hcd" "hpsa" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+        };
+      };
+    };
+    filesystem = {
+      encryption.enable = true;                 # This line can be removed if not needed as it is already default set by the role template
+      impermanence.enable = true;               # This line can be removed if not needed as it is already default set by the role template
+      swap = {
+        type = "partition";
+      };
+    };
+    hardware = {
+      cpu = "intel";
+      raid.enable = true;                      # This line can be removed if not needed as it is already default set by the role template
+    };
+    network = {
+      dns = {
+        enable = true;
+        servers = [ "1.1.1.1" ];
+        stub = true;
+        hostname = "ms1";
+      };
+      wired = {
+        enable = true;
+        interfaces = {
+          ms1net = {
+           type = "static";
+           ip = "10.0.0.150/24";
+           gateway = "10.0.0.1";
+           mac = "94:57:a5:6b:1c:04";
+          };
+        };
+      };
+      bridges = {
+        br0 = {
+          name = "br0";
+          interfaces = [ "eno1" ];
+           type = "static";
+           ip = "10.0.0.150/24";
+           gateway = "10.0.0.1";
+        };
+      };
+      vpn = {
+        zerotier = {
+          enable = true;
+          networks = [
+            "e5cd7a9e1cfbc9a8"
+          ];
+          port = 9993;
+        };
+      };
+#      firewall = {
+#        opensnitch.enable = false;
+#      };
+    };
+    role = "server";
+    user = {
+      root.enable = true;
+      xavier.enable = true;
+      sam.enable = true;
+    };
+    service = {
+      vscode_server.enable = true;
+    };
+  };
+  nixpkgs.hostPlatform = "x86_64-linux";
+}
---
+++ hosts/ms2/default.nix
@@ -0,0 +1,80 @@
+{ config, inputs, pkgs, ...}: {
+
+  imports = [
+    inputs.disko.nixosModules.disko
+    ./disks.nix
+    ../common
+  ];
+
+  host = {
+    feature = {
+      boot = {
+        initrd = {
+          modules = [ "xhci_pci" "ehci_pci" "uhci_hcd" "hpsa" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+        };
+      };
+    };
+    filesystem = {
+      encryption.enable = true;                 # This line can be removed if not needed as it is already default set by the role template
+      impermanence.enable = true;               # This line can be removed if not needed as it is already default set by the role template
+      swap = {
+        type = "partition";
+      };
+    };
+    hardware = {
+      cpu = "intel";
+      raid.enable = true;                      # This line can be removed if not needed as it is already default set by the role template
+    };
+    network = {
+      dns = {
+        enable = true;
+        servers = [ "1.1.1.1" ];
+        stub = true;
+        hostname = "ms2";
+      };
+      wired = {
+        enable = true;
+        interfaces = {
+          ms1net = {
+           type = "static";
+           ip = "10.0.0.151/24";
+           gateway = "10.0.0.1";
+           mac = "94:57:a5:52:6d:2c";
+          };
+        };
+      };
+      bridges = {
+        br0 = {
+          name = "br0";
+          interfaces = [ "eno1" ];
+           type = "static";
+           ip = "10.0.0.151/24";
+           gateway = "10.0.0.1";
+        };
+      };
+      vpn = {
+        zerotier = {
+          enable = true;
+          networks = [
+            "e5cd7a9e1cfbc9a8"
+          ];
+          port = 9993;
+        };
+      };
+#      firewall = {
+#        opensnitch.enable = false;
+#      };
+    };
+    role = "server";
+    user = {
+      root.enable = true;
+      xavier.enable = true;
+      sam.enable = true;
+    };
+    service = {
+      vscode_server.enable = true;
+    };
+  };
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+}
